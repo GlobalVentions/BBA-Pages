fbfd8eaa46e893a6f34a3abcb708397ce681b4c3
Bhavik Mehta <bhavik.mehta@techcronus.com>
2018-07-03 20:09:46 +0530

    Added new HTML page
    Added New HTML Page -
    	289180128.html
    	289180969.html
    	289180181.html
    	289180314.html
    	289181012.html
    	289179885.html
    	289180339.html

--- js/style.js
+++ js/style.js
@@ -761,4 +761,19 @@ $(document).ready(function()
             $('#click').html('Done');
         }
     });
+
+    /* Js code for smooth scrolling */
+    $(document).ready(function(){
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
 });
