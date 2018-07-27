0e904f7c9df1d93692d9f063e1b7f1f0f8b1f829
Bhavik Mehta <bhavik.mehta@techcronus.com>
2018-07-27 19:37:27 +0530

    Added a New HTML
    Added 9 New HTML
        1) 289181702.html
        2) 289181704.html
        3) 289181705.html
        4) 289180640.html
        5) 289180644.html
        6) 289180669.html
        7) 289180673.html
        8) 289180700.html
        9) 289180710.html

--- js/style.js
+++ js/style.js
@@ -894,6 +894,22 @@ $(document).ready(function()
             $(".funding-rate").addClass("d-none");
         });
     });
+    /* Js Code for input spinner */
+    $(document).ready(function(){
+            $('#spinner button').on('click', function(){
+                let input = $(this).closest('#spinner').find('input[name=qty]');
+
+                if($(this).data('action') === 'increment') {
+                    if(input.attr('max') === undefined || parseInt(input.val()) < parseInt(input.attr('max'))) {
+                        input.val(parseInt(input.val(), 10) + 1);
+                    }
+                } else if($(this).data('action') === 'decrement') {
+                    if(input.attr('min') === undefined || parseInt(input.val()) > parseInt(input.attr('min'))) {
+                        input.val(parseInt(input.val(), 10) - 1);
+                    }
+                }
+            });
+    });
     /* JS code for hide show div based on Radio button in Trending Calculators*/
     $(function() {
         $('input[name=have-credit-card]').on('click init-post-format', function() {
